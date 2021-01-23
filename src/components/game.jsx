83b903070c3806f7f28775daf6b9abf13b4f7066
Sajeeb <sajeebahamed6@gmail.com>
2021-01-24 03:15:15 +0600

    state organized

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import Board from './board';
 
+function calculateWinner(squares) {
+    return
+}
 class Game extends React.Component {
     state = {
-        history: [{ squares: Array(9).fill(null) }]
+        history: [{ squares: Array(9).fill(null) }],
+        stepNum: 0,
+        xIsNext: true
     }
-    onClick = i => {
-        console.log(i);
+    handleClick = i => {
+        const history = this.state.history.slice(0, this.state.stepNum + 1)
+        const current = history[history.length - 1]
+        const squares = current.squares.slice()
+        
+        if(calculateWinner(squares) || squares[i]) {
+            return
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            history: history.concat([{ squares }]),
+            stepNum: history.length,
+            xIsNext: !this.state.xIsNext
+        })
     }
     render() {
+        console.log(this.state);
         return (
             <div>
                 <h1> Tic Tac Toe </h1>
-                <Board onClick={this.onClick} squares={this.state.history[0]} />
+                <Board onClick={this.handleClick} squares={this.state.history[0]} />
             </div>
         )
     }
