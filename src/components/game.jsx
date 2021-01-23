3c51c5f9499040640d58b6253adc1ebaaa036ca8
Sajeeb <sajeebahamed6@gmail.com>
2021-01-24 03:32:52 +0600

    winning logice

--- src/components/game.jsx
+++ src/components/game.jsx
@@ -2,7 +2,23 @@ import React from 'react';
 import Board from './board';
 
 function calculateWinner(squares) {
-    return
+    const lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6],
+    ]
+    for(let i=0;i<lines.length; i++) {
+        const [a,b,c] = lines[i]
+        if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null
 }
 class Game extends React.Component {
     state = {
@@ -26,11 +42,14 @@ class Game extends React.Component {
         })
     }
     render() {
-        console.log(this.state);
+        const history = this.state.history
+        const current = history[this.state.stepNum]
+        const winner = calculateWinner(current.squares)
+        console.log(winner);
         return (
             <div>
                 <h1> Tic Tac Toe </h1>
-                <Board onClick={this.handleClick} squares={this.state.history[0]} />
+                <Board onClick={this.handleClick} squares={current.squares} />
             </div>
         )
     }
