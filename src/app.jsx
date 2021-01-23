cbb31de550bfc7cdb42e22e43665c9c0e0f2abcc
Sajeeb <sajeebahamed6@gmail.com>
2021-01-24 02:53:05 +0600

    structure with component

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+import Game from './components/game'
+const App = () => {
+    return (
+        <div className='app'>
+            <h1> tic tac to </h1>
+            <Game />
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/game.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Board from './board';
+
+class Game extends React.Component {
+    state = {
+        history: [{ squares: Array(9).fill(null) }]
+    }
+    onClick = i => {
+        console.log(i);
+    }
+    render() {
+        return (
+            <div>
+                <h1> Tic Tac Toe </h1>
+                <Board onClick={this.onClick} squares={this.state.history[0]} />
+            </div>
+        )
+    }
+}
+export default Game
